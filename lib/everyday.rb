ec1260f4217cfe0ec635e4b0d539f189cb46506f
netalpha <cs.chavez1986@gmail.com>
2014-04-15 17:24:53 +0100

    add .ssh autoinstall

--- lib/everyday.rb
+++ lib/everyday.rb
@@ -2,3 +2,10 @@ require 'everyday/version.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
+#
+
+def prompt_install_msg app_name
+  puts "===================================="
+  puts "Install: #{app_name}"
+  puts "===================================="
+end
